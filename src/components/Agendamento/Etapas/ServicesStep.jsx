8e8dc0860efd0c48493d326d6d65d3957e00e6f6
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2025-02-10 14:43:13 -0300

    adicionando consumo de api para exibir serviços

--- src/components/Agendamento/Etapas/ServicesStep.jsx
+++ src/components/Agendamento/Etapas/ServicesStep.jsx
@@ -1,17 +1,48 @@
 import CustomCard from "@/components/UI-Components/CustomCard";
-import servicesData from '@/assets/data/servicesData';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ServicesStep = () => {
+  const [services, setServices] = useState([])
+  const [filter, setFilter] = useState("Todos");
+  const getServices = async () => {
+    const res = await axios.get('http://localhost:3000/api/servicos');
+    setServices(res.data)
+  }
+
+  useEffect(() => {
+    getServices()
+  }, []);
+
+  const filteredServices = services.filter((service) => {
+    if (filter === "Todos") {
+      return true; // Exibe todos os serviços
+    }
+    return service.category === filter; // Filtra pela categoria
+  });
+
   return (
-    <div>
+    <div className="my-20">
+      <div className="flex gap-1 justify-end">
+        <span className="font-semibold mt-1">Ordenar por:</span>
+        <select className="border rounded-md border-gray-300 p-1 mb-2" name="orderService" id="orderService" value={filter} onChange={(e) => setFilter(e.target.value)}>
+          <option value="Todos">Todos</option>
+          <option value="Facial">Facial</option>
+          <option value="Corporal">Corporal</option>
+        </select>
+      </div>
       <div className="flex flex-wrap justify-center gap-6">
-        {servicesData.services.map((service, index) => (
+        {filteredServices.map((service) => (
           <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
             <CustomCard
-              key={index}
+              key={service.id}
               title={service.title}
-              description={s.description}
+              description={service.description}
               image={service.image}
+              duration={service.duration + " min"}
+              price={"R$" + service.price}
+              buttonText={"Agendar"}
+              icon={true}
             />
           </div>
         ))}
--- src/components/UI-Components/CustomCard.jsx
+++ src/components/UI-Components/CustomCard.jsx
@@ -2,14 +2,14 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../ui/card
 import { TimerIcon } from "lucide-react";
 import { Button } from "../ui/button";
 
-const CustomCard = ({ title, description, image, price, duration, buttonText, onClick, icon }) => {
+const CustomCard = ({ title, description, image, price, category, duration, buttonText, onClick, icon }) => {
   return (
-    <Card className="w-full shadow-md rounded-lg">
+    <Card className="w-full shadow-md rounded-lg flex flex-col h-full"> {/* Adicionei o flex e h-full aqui */}
       {image && <img src={image} alt={title} className="w-full h-40 object-cover" />}
       <CardHeader>
         <CardTitle className="text-lg">{title}</CardTitle>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex-1"> {/* Adicionei flex-1 para permitir que o conteúdo ocupe o restante do espaço */}
         <p className="text-gray-600 text-sm">{description}</p>
         <div className="flex justify-between items-center mt-4">
           <span className="text-lg font-bold text-primary">{price}</span>
